313f21c58fbe391006924a58339121f4d933a342
bdew <bdew@bdew.net>
2026-01-16 21:24:57 +0200

    add option to only process new images

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 import fs from "fs";
 import path from "path";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 import { parseConfig } from "./files/config";
-import { processImage } from "./process";
 import { parseVersions, saveVersions, Versions } from "./files/versions";
 import { GitHandler } from "./git";
+import { processImage } from "./process";
 
 async function main(): Promise<void> {
-  if (process.argv.length !== 3) {
-    console.error("Usage:");
-    console.error(" dockver <configFile>");
-    process.exit(1);
-  }
+  const args = await yargs(hideBin(process.argv))
+    .usage("Usage: $0 <configFile>")
 
-  const configFilePath = path.resolve(process.argv[2]);
+    .option("only-new", { alias: "n", type: "boolean", desc: "Only process new images" })
+    .demandCommand(1, 1)
+    .parse();
+
+  const configFilePath = path.resolve(args._[0].toString());
   console.log("Read config from", configFilePath);
   const configFile = fs.readFileSync(configFilePath, "utf-8");
   const config = parseConfig(configFile);
@@ -34,18 +37,21 @@ async function main(): Promise<void> {
     process.exit(1);
   }
 
-  const toRemove = Object.keys(versions).filter(name => !config.images[name]);
-  for (const name of toRemove) {
-    console.log("Removing image that's not in config:", name);
-    const prev = versions[name];
-    delete versions[name];
-    const versionsData = saveVersions(versions, config.output.format);
-    fs.writeFileSync(versionsFilePath, versionsData, "utf-8");
-    if (git)
-      await git.commitChange(prev, undefined);
+  if (!args["only-new"]) {
+    const toRemove = Object.keys(versions).filter(name => !config.images[name]);
+    for (const name of toRemove) {
+      console.log("Removing image that's not in config:", name);
+      const prev = versions[name];
+      delete versions[name];
+      const versionsData = saveVersions(versions, config.output.format);
+      fs.writeFileSync(versionsFilePath, versionsData, "utf-8");
+      if (git)
+        await git.commitChange(prev, undefined);
+    }
   }
 
   for (const [name, image] of Object.entries(config.images)) {
+    if (args["only-new"] && versions[name]) continue;
     const res = await processImage(name, image, versions[name] ?? null);
     if (res) {
       const prev = versions[name];
